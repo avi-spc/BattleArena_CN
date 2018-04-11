2dc2d89ab99122e65e5aeafc20a529f25721e8be
avi-spc <avdhesh7394vn@gmail.com>
2018-04-11 16:36:48 +0530

    UI

--- Assets/Scripts/CreateRoom.cs
+++ Assets/Scripts/CreateRoom.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class CreateRoom : MonoBehaviour {
 
+    public string arenaCreationStatus;
+    public GameObject LobNet;
+    LobbyNetwork LN;
+
     [SerializeField]
     private Text _roomName;
     private Text RoomName {
@@ -12,6 +16,15 @@ public class CreateRoom : MonoBehaviour {
     }
 
     //public bool con=false;
+    private void Awake()
+    {
+        LobNet = GameObject.FindGameObjectWithTag("LN");
+    }
+    private void Start()
+    {
+        LN = LobNet.GetComponent<LobbyNetwork>();
+        
+    }
 
     public void OnCreateRoom() {
 
@@ -21,9 +34,11 @@ public class CreateRoom : MonoBehaviour {
         roomOptions.EmptyRoomTtl = 6000;
 
         if (PhotonNetwork.CreateRoom(RoomName.text,roomOptions,TypedLobby.Default)) {
+            arenaCreationStatus = "Arena creation request sent successfully.";
             Debug.Log("Request for room creation sent successfully.");
         }
         else {
+            arenaCreationStatus = "Arena creation request failed";
             Debug.Log("Request for room creation failed.");
         }
 
@@ -33,19 +48,24 @@ public class CreateRoom : MonoBehaviour {
     }
 
     private void OnPhotonCreateRoomFailed(object[] codeAndMessage) {
-
+        arenaCreationStatus = "Arena creation failed";
         Debug.Log("Room creation failed : " + codeAndMessage);
 
     }
 
     private void OnCreatedRoom() {
-
+        arenaCreationStatus = "Arena created successfully";
         Debug.Log("Room created successfully.");
 
+    }
+
+    private void Update()
+    {
+        LN.arenaStatusText.text = arenaCreationStatus;
     }
 
     //private void idf() {
     //    PlayerNetwork.Instance.eachPlayerName[((PhotonNetwork.player.ID) - 1) % 5] = PhotonNetwork.playerName;
     //}
-	
+
 }
--- Assets/Scripts/LobbyNetwork.cs
+++ Assets/Scripts/LobbyNetwork.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyNetwork : MonoBehaviour {
 
+    public string status;
+    public Text lobbyStatusText, arenaStatusText;
 	// Use this for initialization
 	private void Start () {
 
         if (!PhotonNetwork.connected) {
+
+            status = "Connecting to server . . .";
 
             Debug.Log("Connecting to server . . .");
             PhotonNetwork.ConnectUsingSettings("0.0.0.0");
@@ -16,6 +21,8 @@ public class LobbyNetwork : MonoBehaviour {
     }
 
     private void OnConnectedToMaster() {
+
+        status = "Connected to master.";
 
         Debug.Log("Connected to master.");
         PhotonNetwork.automaticallySyncScene = true;
@@ -27,12 +34,18 @@ public class LobbyNetwork : MonoBehaviour {
 
     private void OnJoinedLobby() {
 
+        status = "Lobby Joined";
+
         Debug.Log("Lobby Joined");
 
         if(!PhotonNetwork.inRoom)
             MainCanvasManager.Instance.LobbyCanvas.transform.SetAsLastSibling();
 
     }
-	
-	
+
+    private void Update()
+    {
+        lobbyStatusText.text = status;
+    }
+
 }
